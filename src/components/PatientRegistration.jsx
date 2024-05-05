e6b74677a2d734f28aae2745ceafe2e925fbe18e
Sarthak543 <sarthakjaiswal543@gmail.com>
2024-05-05 17:17:59 +0530

    Patient Registration Form

--- src/components/PatientRegistration.jsx
+++ src/components/PatientRegistration.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import documentContext from '../context/Document_State/DocumentContext'
-import { Link } from 'react-router-dom';
 
 export default function PatientRegistration() {
     const backgroundStyle = {
@@ -17,11 +16,40 @@ export default function PatientRegistration() {
         filter: 'blur(8px)'
     };
 
-    const { docData, setDocData } = useContext(documentContext);
-    const { signIn, setSignIn } = useContext(documentContext);
+    const { patData, setPatData } = useContext(documentContext);
     const onChangeHandler = (e) => {
-        setDocData({ ...docData, [e.target.name]: e.target.value })
+        setPatData({ ...patData, [e.target.name]: e.target.value })
     }
+
+    const handleSubmit= async(event)=>{
+        event.preventDefault();
+        const formData = new FormData();
+
+        //appending all the data from patData to formData
+        Object.keys(patData).forEach(key =>{
+            formData.append(key,patData[key]);
+        });
+
+        //Including the file if uploaded
+        const fileInput = document.querySelector('#inputGroupFile02');
+        if(fileInput.files[0]){
+            formData.append('image',fileInput.files[0]);
+        }
+
+        // API call
+        try {
+            const response = await fetch('http://localhost:8010/patientRegistration',{
+                method:'post',
+                body:formData
+            });
+            if(response.ok){
+                alert("Successfully Registered");
+            }
+        } catch (error) {
+            alert('Error occur');
+            console.error(error);
+        }
+    };
 
     return (
         <>
@@ -31,16 +59,16 @@ export default function PatientRegistration() {
                 <div className='p-4 w-75 shadow-lg p-3 mb-5 rounded' style={{ background: 'rgba(0,0,0,0)' }} action="">
                     <h1 className='pb-1'>Registration</h1>
                     <p className='fs-6' style={{ fontFamily: 'serif' }}>Fill in the below data</p>
-                    <form action="">
-                        <input style={{ background: 'rgba(0,0,0,0)' }} type="text" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='Full Name ' name='name' value={docData.name} onChange={onChangeHandler} />
-                        <input style={{ background: 'rgba(0,0,0,0)' }} type="email" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='E-mail address ' name='email' value={docData.email} onChange={onChangeHandler} />
-                        <input style={{ background: 'rgba(0,0,0,0)' }} type="number" maxLength="12" title="Ten digits code" required className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='Mobile ' name='mobile' value={docData.mobile} onChange={onChangeHandler} />
-                        <input style={{ background: 'rgba(0,0,0,0)' }} type="password" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='Password ' name='password' value={docData.password} onChange={onChangeHandler} />
+                    <form onSubmit={handleSubmit} action="/patientRegistration" method='post' encType='multipart/form-data'>
+                        <input style={{ background: 'rgba(0,0,0,0)' }} type="text" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='Full Name ' name='name' value={patData.name} onChange={onChangeHandler} />
+                        <input style={{ background: 'rgba(0,0,0,0)' }} type="email" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='E-mail address ' name='email' value={patData.email} onChange={onChangeHandler} />
+                        <input style={{ background: 'rgba(0,0,0,0)' }} type="number" maxLength="12" title="Ten digits code" required className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='Mobile ' name='mobile' value={patData.mobile} onChange={onChangeHandler} />
+                        <input style={{ background: 'rgba(0,0,0,0)' }} type="password" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='Password ' name='password' value={patData.password} onChange={onChangeHandler} />
                         <input style={{ background: 'rgba(0,0,0,0)' }} type="password" className='form-control m-2 mx-auto border-bottom border-1 border-dark' placeholder='confirm password ' />
 
                         <div className="form-check-inline w-100">
                             <div className="input-group">
-                                <input  style={{ background: 'rgba(0,0,0,0)' }} type="file" className="form-control" id="inputGroupFile02" />
+                                <input  style={{ background: 'rgba(0,0,0,0)' }} type="file" className="form-control" id="inputGroupFile02" name='image' />
                                 <label className="input-group-text" htmlFor="inputGroupFile02">Upload Profile photo</label>
                             </div>
                         </div>
@@ -62,7 +90,7 @@ export default function PatientRegistration() {
                                 <input className="form-check-input" type="radio" id="Others" value="Others" name="gender" onChange={onChangeHandler} />
                             </div>
                         </div>
-                        <Link className='btn btn-primary w-25 offset-9' to='/DocterRegistration/form2' type='button'>Submit</Link>
+                        <button className='btn btn-primary w-25 offset-9' type='submit'>Submit</button>
 
                     </form>
                 </div>
