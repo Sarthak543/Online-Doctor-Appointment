514ff605d561c455c3906d321adc6c34aa94aba8
Sarthak543 <sarthakjaiswal543@gmail.com>
2024-05-15 19:17:38 +0530

    improve landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,11 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const a = useContext(documentContext);
-  const { bg, setbg } = a;
   const { signIn, setSignIn } = a;
-
-  useEffect(() => {
-    setbg("../HomePage.png");
-  }, []);
 
   useEffect(() => {
     console.log("Default value is set to : ", signIn);
   }, []);
-
-  const imageStyle = {
-    height: "28vh",
-    width: "15vw",
-    cursor: "pointer",
-  };
 
   const signInHandler = async (event) => {
     const name = event.target.name;
@@ -30,25 +19,47 @@ export default function Home() {
   return (
     <>
       <div className="home_div_bg">
+        <div className=" green-bg"></div>
         <img className="img_bg" src="../HomePage.png" alt="" srcset="" />
       </div>
       <div className="container text-center d-flex justify-content-center align-items-center flex-column gap-4 mt-4">
         <span style={{ fontSize: "7vh" }}>Online Doctor Appointment</span>
-        <h3 className=" text-center">Sign in As</h3>
-        <div>
-          <Link to={"PatientRegistration"}>
-            <button className="btn btn-outline-success me-4">
-              I'm Patient
-            </button>
-          </Link>
-          <Link to={"DocterRegistration/form1"}>
-            <button className="btn btn-outline-primary">I'm Doctor</button>
-          </Link>
-        </div>
       </div>
-      <p className="text-center mt-3">
-        <Link to={"Register"}>Dont have account? Click here to Register</Link>
-      </p>
+
+      <div
+        className="d-flex flex-column ms-5 w-50"
+        style={{ marginTop: "10vh" }}
+      >
+        <p className="fs-1 mb-0">Get Quick</p>
+        <p className="fs-1 fw-semibold">Medical Service</p>
+        <p className="text-secondary">
+          In today's fast-paced world,access to promt and efficient medical
+          service is of paramount importance. When faced with a medical
+          emergency or seeking immediate medical attention, the ability to
+          receive quick medical services can significantly impact the outcome of
+          a situation
+        </p>
+      </div>
+      <div className="ms-5">
+        <Link to={"signIn"}>
+          <button
+            className="btn btn-outline-success me-4"
+            name="patient"
+            onClick={signInHandler}
+          >
+            I'm Patient
+          </button>
+        </Link>
+        <Link to={"signIn"}>
+          <button
+            className="btn btn-outline-primary"
+            name="Doctor"
+            onClick={signInHandler}
+          >
+            I'm Doctor
+          </button>
+        </Link>
+      </div>
     </>
   );
 }
