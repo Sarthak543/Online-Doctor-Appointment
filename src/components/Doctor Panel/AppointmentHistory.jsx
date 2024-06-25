e9332ea9bee74f2a51fe0447bce93dcefd0772d2
Sarthak543 <sarthakjaiswal543@gmail.com>
2024-06-25 13:34:18 +0530

    backend added

--- src/components/Doctor Panel/AppointmentHistory.jsx
+++ src/components/Doctor Panel/AppointmentHistory.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function AppointmentHistory() {
+  const [appointment, setappointment] = useState([]);
+
+  useEffect(() => {
+    async function getAppointments() {
+      try {
+        let a = "Rakesh Goyal";
+        const response = await fetch(
+          `http://localhost:8010/DoctorAppointment/${a}`,
+          {
+            method: "post",
+          }
+        );
+
+        const data = await response.json();
+        console.clear();
+        console.log("asdfghjkl;'asdfghjkl;'       ", data);
+        setappointment(data);
+      } catch (error) {
+        console.clear();
+        console.log(error);
+      }
+    }
+
+    getAppointments();
+  }, []);
+
   return (
     <>
       <div className="container text-center fs-2 fw-bold">
         Consultant History
       </div>
+      {appointment.length > 0 && ( // Check if appointments exist
       <div className="container w-75">
         <div className="border-top rounded-2 mt-5">
           {/* Consultant table */}
@@ -20,105 +47,34 @@ export default function AppointmentHistory() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill text-end me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill text-end">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill text-end me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill text-end">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill text-end me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill text-end">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">4</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill">Delete</button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">5</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill text-end me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill text-end">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">6</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill text-end me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill text-end">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">7</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                <td className="w-25">
-                  <button className="btn btn-outline-success badge-pill text-end me-3">
-                    Open
-                  </button>
-                  <button className="btn btn-outline-danger badge-pill text-end">
-                    Delete
-                  </button>
-                </td>
-              </tr>
+              {appointment.map((item, index) => (
+                <tr>
+                  <th scope="row">{index+1}</th>
+                  <td>{item.patientName}</td>
+                  <td>{item.date.substring(0,10)}</td>
+                  <td>{item.patientNumber}</td>
+                  <td className="w-25">
+                    <button className="btn btn-outline-success badge-pill text-end me-3">
+                      Open
+                    </button>
+                    <button className="btn btn-outline-danger badge-pill text-end">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
-        <div className="d-flex justify-content-between" style={{marginBottom:'15vh'}}>
+        <div
+          className="d-flex justify-content-between"
+          style={{ marginBottom: "15vh" }}
+        >
           <button className="btn btn-primary m-2">Previous</button>
           <button className="btn btn-primary m-2">Next</button>
         </div>
       </div>
+      )}
     </>
   );
 }
