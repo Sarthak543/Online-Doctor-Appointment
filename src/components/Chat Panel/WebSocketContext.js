5a709eddb2a9c6f7cdb1359da83c6c8556e09ffb
Sarthak543 <sarthakjaiswal543@gmail.com>
2024-07-02 16:26:48 +0530

    context for webSocket

---
+++ src/components/Chat Panel/WebSocketContext.js
@@ -0,0 +1,27 @@
+// WebSocketContext.js
+import React, { createContext, useContext, useState } from 'react';
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+const WebSocketContext = createContext(null);
+
+export const WebSocketProvider = ({ children }) => {
+    const [stompClient, setStompClient] = useState(null);
+
+    const connect = () => {
+        const socket = new SockJS('http://localhost:8010/server1');
+        const client = Stomp.over(socket);
+        client.connect({}, (frame) => {
+            console.log('Connected: ' + frame);
+            setStompClient(client);
+        });
+    };
+
+    return (
+        <WebSocketContext.Provider value={{ stompClient, connect }}>
+            {children}
+        </WebSocketContext.Provider>
+    );
+};
+
+export const useWebSocket = () => useContext(WebSocketContext);
