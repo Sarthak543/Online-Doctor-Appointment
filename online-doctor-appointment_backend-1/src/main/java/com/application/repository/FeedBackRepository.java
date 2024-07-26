777a55f3568912b28bf47c5f7aa8650e37e555b7
Sarthak Jaiswal <99017726+Sarthak543@users.noreply.github.com>
2024-07-26 22:22:37 +0530

    Delete online-doctor-appointment_backend-1 directory

--- online-doctor-appointment_backend-1/src/main/java/com/application/repository/FeedBackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.entity.FeedBack;
-
-public interface FeedBackRepository extends JpaRepository<FeedBack, Long> {
-	
-}
